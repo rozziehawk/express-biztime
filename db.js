a19eca5972b81a1deb3c65056c40c0e6d50b9547
Karl Haakonsen <cityhawk@pobox.com>
2024-01-10 16:47:37 -0500

    Will not start

---
+++ db.js
@@ -0,0 +1,23 @@
+/** Database setup for BizTime. */
+
+const { Client } = require("pg") 
+/*"test") {
+  DB_URI = "postgresql:///BizTime_test";
+} else {
+  DB_URI = "postgresql:///BizTime";
+}*/
+
+// None of these connoect strings work: 
+
+//DB_URI = "postgresql://karl:Bonfire500@localhost:5432/BizTime"
+
+//DB_URI = "postgresql://karl:Bonfire500@localhost:3100/biztime"
+DB_URI = "postgresql:///biztime";
+
+let db = new Client({
+  connectionString: DB_URI
+});
+
+db.connect();
+
+module.exports = db;
